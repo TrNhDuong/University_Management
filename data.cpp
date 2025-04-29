ba063d29965e73a9956726244ec81882c967af00
HarryNguyen <ngduong5124@gmail.com>
2025-04-29 15:48:04 +0000

    Make IDatabase, factory in data.h

--- data.cpp
+++ data.cpp
@@ -1,72 +1,17 @@
 #include "data.h"
+#include "baseEntity.h"
 #include <fstream>
 #include <iostream>
 using std::cout;
 using std::fstream;
 
-vector<Student> StudentDatabase::_dataBase;
+vector<Student> StudentDatabase::_data;
+vector<Lecturer> LecturerDatabase::_data;
 
-int StudentDatabase::getSize() const{
-    return _dataBase.size();
+string StudentDatabase::getDataType() const {
+    return "Student";
 }
 
-Student& StudentDatabase::getData(const int& index) const {
-    return _dataBase[index];
+string LecturerDatabase::getDataType() const {
+    return "Lecturer";
 }
-
-void StudentDatabase::add(const Student& value){
-    _dataBase.push_back(value);
-}
-void StudentDatabase::delete_obj(Student& value){
-    //find value and delete it by using vector.erase
-    int i = 0;
-    for (const Student& s : _dataBase){
-         if (s.getId() != value.getId()) {++i;}
-         else break;
-    }
-    if (i <_dataBase.size()) { _dataBase.erase(_dataBase.begin()+i); }
-    else {
-        cout <<"Student with ID: " << value.getId() << " not found!\n"; 
-    }
-}
-
-void StudentDatabaseDisplay::display(const StudentDatabase& studentDtb){
-    int n = studentDtb.getSize();
-    for (int i = 0; i < n; i++){ 
-        StudentUI::print(studentDtb.getData(i));
-    }
-}
-
-Student StudentDatabaseGetData::readLine(const string& inputLine){
-    int len = inputLine.size();
-    vector<string> list;
-    string temp = "";
-    for (int i = 0; i < len; i++)
-        if (inputLine[i] == '|'){
-            list.push_back(temp);
-            temp = "";
-        } else {
-            temp += inputLine[i];
-        }
-    list.push_back(temp);
-    Student ans;
-    ans.setName(list[0]);
-    ans.setId(list[1]);
-    ans.setBirth(list[2]);
-    ans.setGPA(stof(list[3]));
-    ans.setCredit(stoi(list[4]));
-    return ans;
-}
-
-StudentDatabase StudentDatabaseGetData::read(const string& inputFile){
-    fstream file(inputFile, std::ios::in);
-    StudentDatabase studentDTB;
-    string line;
-    while (getline(file, line)){
-        Student value = readLine(line);
-        studentDTB.add(value);
-    }
-    file.close();
-    return studentDTB;
-}
-
--- data.h
+++ data.h
@@ -1,1 +1,49 @@
-//comment for branch
+#ifndef _DATA_H_
+#define _DATA_H_
+#include <vector>
+#include "student.h"
+#include "lecturer.h"
+#include"faculty.h"
+#include <string>
+using std::vector;
+using std::string;
+
+
+class IDatabase{
+public:
+    virtual string getDataType() const = 0;
+    virtual ~IDatabase() = default;
+};
+
+class StudentDatabase: public IDatabase{
+private:
+    static vector<Student> _data;
+public:
+    string getDataType() const;
+    friend class StudentDatabaseDisplay;
+};
+
+
+class LecturerDatabase: public IDatabase{
+private:
+    static vector<Lecturer> _data;
+public:
+    string getDataType() const;
+    friend class LecturerDatabaseDisplay;
+};
+
+
+class FacultyDatabase: public IDatabase{
+private:
+    vector<Faculty> _data;
+public:
+    string getDataType();
+    ~FacultyDatabase();
+    
+    friend class FacultyDatabaseDisplay;
+};
+
+
+
+
+#endif
