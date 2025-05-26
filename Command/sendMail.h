ee4e7cd0f16734f8418d34455918270e0febb83a
TrNhDuong <23120243@student.hcmus.edu.vn>
2025-05-26 02:29:15 +0000

    Update sendmail

---
+++ Command/sendMail.h
@@ -0,0 +1,22 @@
+#ifndef SEND_MAIL_H
+#define SEND_MAIL_H
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+using namespace std;
+
+class MailSV {
+private:
+    static size_t WriteCallback(char *contents, size_t size, size_t nmemb, string *response);
+    string getAPIkey();
+    string inputReceiver();
+    string inputHeader();
+    string inputContent();
+    void sendMail(string to_mail, string subject, string content);
+public:
+    MailSV() = default;
+    void excute();
+    ~MailSV() = default;
+};
+
+#endif
