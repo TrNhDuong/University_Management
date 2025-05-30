fdd5858f2dcb0c1dd8192c8763cb2481d7c24c83
TrNhDuong <23120243@student.hcmus.edu.vn>
2025-05-30 15:01:08 +0000

    Fix search

--- Command/search.cpp
+++ Command/search.cpp
@@ -14,7 +14,7 @@ vector<BaseEntity*> NameSearch::search(IDatabase* database, const string& name){
     vector<BaseEntity*> ans;
     int size = database->getSize();
     for (int i = 0; i < size; i++)
-        if (database->getData(i)->getId().find(name) != string::npos)
+        if (database->getData(i)->getName().find(name) != string::npos)
             ans.push_back(database->getData(i));
     return ans;
 }
